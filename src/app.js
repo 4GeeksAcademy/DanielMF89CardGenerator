fdf91ee50427f4e61b23f03b0e75213019ddb50b
DanielMF89 <139999329+DanielMF89@users.noreply.github.com>
2023-10-22 13:54:30 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const newSuit = generateSuit();
+  const topSuitElem = document.querySelector(".topSuit");
+  const bottomSuitElem = document.querySelector(".bottomSuit");
+  document.querySelector(".number").innerHTML = generateCard();
+  topSuitElem.className = `topSuit ${newSuit}`;
+  bottomSuitElem.className = `bottomSuit ${newSuit}`;
+
+  document.querySelector("#randomCard").addEventListener("click", () => {
+    const newSuit = generateSuit();
+    const topSuitElem = document.querySelector(".topSuit");
+    const bottomSuitElem = document.querySelector(".bottomSuit");
+
+    document.querySelector(".number").innerHTML = generateCard();
+    topSuitElem.className = `topSuit ${newSuit}`;
+    bottomSuitElem.className = `bottomSuit ${newSuit}`;
+  });
 };
+let deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let suitCard = ["diam", "heart", "spade", "club"];
+function generateCard() {
+  const deckIndex = Math.floor(Math.random() * deck.length);
+  return `${deck[deckIndex]}`;
+}
+
+function generateSuit() {
+  const suitIndex = Math.floor(Math.random() * suitCard.length);
+  return `${suitCard[suitIndex]}`;
+}
